98c90d5f1374855256553ae7af53545f88e4194e
Ashu <ashu.ask.singh@gmail.com>
2022-07-15 21:29:55 +0530

    add Employee.js

---
+++ src/components/Employee.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Employee = ({ employee }) => {
+  return (
+    <tr key={employee.id}>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.firstName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.lastName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.emailId}</div>
+      </td>
+      <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
+        <div className="text-sm text-gray-500">
+          <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">
+            Edit
+          </a>
+          <a href="#" className="text-indigo-600 hover:text-indigo-800">
+            Delete
+          </a>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default Employee;
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,79 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import EmployeeService from '../services/EmployeeService';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import EmployeeService from "../services/EmployeeService";
+import Employee from "./Employee";
 
 const EmployeeList = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [employees, setEmployees] = useState(null);
 
-    const navigate =  useNavigate();
-    const [loading, setLoading] = useState(true);
-    const [employees, setEmployees] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await EmployeeService.getEmployees();
+        setEmployees(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
 
-    useEffect(() => {
-      const fetchData = async () => {
-        setLoading(true);
-        try {
-            const response = await EmployeeService.getEmployees();
-            setEmployees(response.data);
-        } catch(error) {
-            console.log(error);
-        }
-        setLoading(false);
-      };
-
-      fetchData();
-    }, []);
-    
+    fetchData();
+  }, []);
 
   return (
     <div className="container mx-4 my-8">
-        <div className="h-12">
-            <button onClick={() => navigate("/addEmployee")} className="rounded bg-slate-600 text-white px-6 py-2 font-semibold hover:bg-slate-900">
-                Add Employee
-            </button>
-        </div>
-        <div className="flex shadow border-b">
-            <table className="min-w-full">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">First Name</th>
-                        <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">Last Name</th>
-                        <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">Email ID</th>
-                        <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-6">Actions</th>
-                    </tr>
-                </thead>
-                {!loading && (
-                    <tbody className="bg-white">
-                        {employees.map((employee) => (
-                            <tr key={employee.id}>
-                                <td className="text-left px-6 py-4 whitespace-nowrap">
-                                    <div className="text-sm text-gray-500">
-                                        {employee.firstName}
-                                    </div>
-                                </td>
-                                <td className="text-left px-6 py-4 whitespace-nowrap">
-                                    <div className="text-sm text-gray-500">
-                                        {employee.lastName}
-                                    </div>
-                                </td>
-                                <td className="text-left px-6 py-4 whitespace-nowrap">
-                                    <div className="text-sm text-gray-500">
-                                        {employee.emailId}
-                                    </div>
-                                </td>
-                                <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-                                    <div className="text-sm text-gray-500">
-                                        <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">Edit</a>
-                                        <a href="#" className="text-indigo-600 hover:text-indigo-800">Delete</a>
-                                    </div>
-                                </td>
-                            </tr>))
-                        }
-                    </tbody>)
-                }
-            </table>
-        </div>
+      <div className="h-12">
+        <button
+          onClick={() => navigate("/addEmployee")}
+          className="rounded bg-slate-600 text-white px-6 py-2 font-semibold hover:bg-slate-900"
+        >
+          Add Employee
+        </button>
+      </div>
+      <div className="flex shadow border-b">
+        <table className="min-w-full">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                First Name
+              </th>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Last Name
+              </th>
+              <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Email ID
+              </th>
+              <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-6">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          {!loading && (
+            <tbody className="bg-white">
+              {employees.map((employee) => (
+                <Employee employee={employee} key={employee.id}></Employee>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default EmployeeList;
